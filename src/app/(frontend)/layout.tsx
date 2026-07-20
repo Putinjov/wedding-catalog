dd25f785a612a35d9f7d9faf0167e85b808f7454
Putinjov <114943911+Putinjov@users.noreply.github.com>
2026-07-20 21:50:41 +0100

    Remove SVG favicon link from layout
    Removed SVG favicon link from layout.

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -46,7 +46,6 @@ export default async function RootLayout({ children }: { children: React.ReactNo
     >
       <head>
         <link href="/favicon.ico" rel="icon" sizes="32x32" />
-        <link href="/favicon.svg" rel="icon" type="image/svg+xml" />
       </head>
       <body>
         <Providers>
